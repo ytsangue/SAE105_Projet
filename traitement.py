86681edf1208bf65d833013a2556cf9d9b5fb0b0
LouisGVC <152868387+LouisGVC@users.noreply.github.com>
2024-01-05 22:22:45 +0100

    Update traitement.py

--- traitement.py
+++ traitement.py
@@ -5,20 +5,20 @@ import matplotlib.pyplot as plt
 table = []
 with open('ADECal.csv', newline='') as csvfile:
     reader = csv.reader(csvfile, delimiter=',')
-    for row in reader:
-        table.append(row)
+    for ligne in reader:
+        table.append(ligne)
 
 # Suppression des \n
 for chaine in table:
     chaine[3] = chaine[3].replace('\n', ',')
 
-prof = input("Entrez le nom d'un.e enseignant.e : ")
+prof = input("Entrez le nom d'un enseignant : ")
 prof = prof.upper()
 
-def calcule_duree_prof(t: list[int]):
+def calcule_duree_prof(t: list[int]) -> float:
     format_date = "%Y-%m-%d %H:%M:%S%z"
-    date_debut = liste[1]
-    date_fin = liste[2]
+    date_debut = t[1]
+    date_fin = t[2]
     heures_debut, minutes_debut = map(int, date_debut.split()[1].split(':')[:2])
     heures_fin, minutes_fin = map(int, date_fin.split()[1].split(':')[:2])
     difference_heures = heures_fin - heures_debut
@@ -26,36 +26,32 @@ def calcule_duree_prof(t: list[int]):
     duree_en_heures = difference_heures + difference_minutes / 60
     return duree_en_heures
 
-ma_liste = table
-module_heures = {}
-heures_CM = 0
-heures_TD = 0
-heures_TP = 0
+def calculer_heures_module(table: list[tuple], prof: str)-> tuple[dict, int, int, int]:   
+    ma_liste = table
+    module_heures = {}
+    heures_CM = 0
+    heures_TD = 0
+    heures_TP = 0
 
-for element in table:
-    if prof in element[3]:
-        module_heures[element[0]]= module_heures.get(element[0],0) + calcule_duree_prof(element)
-        if re.search("Amphi",element[4]) or re.search("DS",element[0]):
-            heures_CM = heures_CM + calcule_duree_prof(element)
-        elif re.search("Labo",element[4]):
-            heures_TP = heures_TP + calcule_duree_prof(element)
-        elif re.search("TD",element[4]):
-            heures_TD = heures_TD + calcule_duree_prof(element)
+    for element in table:
+        if prof in element[3]:
+            module_heures[element[0]]= module_heures.get(element[0],0) + calcule_duree_prof(element)
+            if re.search("Amphi",element[4]) or re.search("DS",element[0]):
+                heures_CM += calcule_duree_prof(element)
+            elif re.search("Labo",element[4]):
+                heures_TP += calcule_duree_prof(element)
+            elif re.search("TD",element[4]) or re.search("CAO",element[4]):
+                heures_TD += calcule_duree_prof(element)
 
-print(module_heures)
-
-
-print("Heures CM :",heures_CM)
-print("Heures TD :",heures_TD)
-print("Heures TP :",heures_TP)
+    return module_heures, heures_CM, heures_TD, heures_TP
 
 heures_ = {'Heures Total de CM' : heures_CM, 'Heures Total TD' : heures_TD, 'Heures Total TP' : heures_TP}
 
 Different_Heures = list(heures_.keys())
 Nombre_heures = list(heures_.values())
 
-modules = list(module_heures.keys())
-heures = list(module_heures.values())
+modules = list(resultats[0].keys())
+heures = list(resultats[0].values())
 
 #Histogramme 
 plt.bar(modules, heures)
